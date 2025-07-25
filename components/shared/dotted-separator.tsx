70513bb0c5b756de9258b9643f2be71565f68184
ddamaja <staystrongbg@gmail.com>
2025-07-25 18:53:30 +0200

    added zod, react hook form and login form handling

--- components/shared/dotted-separator.tsx
+++ components/shared/dotted-separator.tsx
@@ -1,7 +1,46 @@
-export const DottedSeparator = () => {
+import { cn } from '@/lib/utils';
+
+type DottedSeparatorProps = {
+  className?: string;
+  color?: string;
+  height?: string;
+  dotsize?: string;
+  gapsize?: string;
+  direction?: 'horizontal' | 'vertical';
+};
+
+export const DottedSeparator = ({
+  className,
+  color = '#d4d4d8',
+  height = '2px',
+  dotsize = '2px',
+  gapsize = '5px',
+  direction = 'horizontal',
+}: DottedSeparatorProps) => {
+  const isHorizontal = direction === 'horizontal';
+
   return (
-    <div className="flex items-center justify-center my-4">
-      <div className="border-t border-dotted border-gray-300 w-full"></div>
+    <div
+      className={cn(
+        isHorizontal
+          ? 'w-full flex items-center'
+          : 'h-full flex flex-col items-center',
+        className
+      )}
+    >
+      <div
+        className={isHorizontal ? 'flex-grow' : 'flex-grow-0'}
+        style={{
+          width: isHorizontal ? '100%' : height,
+          height: isHorizontal ? height : '100%',
+          backgroundImage: `radial-gradient(circle, ${color} 25%, transparent 25%)`,
+          backgroundSize: isHorizontal
+            ? `${parseInt(dotsize) + parseInt(gapsize)}px ${height}`
+            : `${height} ${parseInt(dotsize) + parseInt(gapsize)}px`,
+          backgroundRepeat: isHorizontal ? 'repeat-x' : 'repeat-y',
+          backgroundPosition: 'center',
+        }}
+      ></div>
     </div>
   );
 };
