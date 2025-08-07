5d926b50955fb08362d7273cae9aa98acf462f18
ddamaja <staystrongbg@gmail.com>
2025-08-07 16:30:11 +0200

    login after registration fixed

--- components/auth/api/use-register.ts
+++ components/auth/api/use-register.ts
@@ -1,17 +1,26 @@
-'use client';
+"use client";
 
-import { useMutation } from '@tanstack/react-query';
-import { client } from '@/lib/rpc';
-import { InferRequestType, InferResponseType } from 'hono';
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { client } from "@/lib/rpc";
+import { InferRequestType, InferResponseType } from "hono";
+import { useRouter } from "next/navigation";
 
-type ResponseType = InferResponseType<(typeof client.api.auth.signup)['$post']>;
-type RequestType = InferRequestType<(typeof client.api.auth.signup)['$post']>;
+type ResponseType = InferResponseType<(typeof client.api.auth.signup)["$post"]>;
+type RequestType = InferRequestType<(typeof client.api.auth.signup)["$post"]>;
 
 export const useRegister = () => {
+  const queryClient = useQueryClient();
+
+  const router = useRouter();
   const mutation = useMutation<ResponseType, Error, RequestType>({
     mutationFn: async ({ json }) => {
       const response = await client.api.auth.signup.$post({ json });
       return await response.json();
+    },
+    onSuccess: () => {
+      // Invalidate the current user query to ensure the user is logged in
+      queryClient.invalidateQueries({ queryKey: ["currentUser"] });
+      router.push("/");
     },
   });
 
